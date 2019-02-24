da234f79e6b0778cb536ac6076fcd3dbd6b469fc
umesh <umeshdataman@hotmail.com>
2019-02-23 22:27:07 -0600

    Update udemy_lists.py

--- udemy_lists.py
+++ udemy_lists.py
@@ -1,4 +1,27 @@
-awesomeList =['Hello', 45, 'hacha',8]
-#print(awesomeList * 8)
-#print(awesomeList + awesomeList)
-print(awesomeList[2:3])
+##"""lists"""
+##awesomeList =['Hello', 45, 'hacha',8]
+##print(awesomeList * 8)
+##print(awesomeList + awesomeList)
+##print(awesomeList[2:3])
+##"""tuple"""
+####"no example"#
+####Almost a list, but difference is, that it is read only, aka can't edit after
+####declared.
+##"""dictionary"""
+
+
+newList = ["yo", "ho", "go"]
+hindi_english = {}
+hindi_english["hello"] = "नमस्कार"
+hindi_english["bye"] = "अलविदा"
+hindi_english["good_morning"] = "सुप्रभात"
+hindi_english["good_night"] = "शुभरात्रि"
+hindi_english["NList"] = newList
+
+
+print(hindi_english.values())
+
+
+
+
+
