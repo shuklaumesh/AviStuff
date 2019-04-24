6cfe6d434d6a2b11c99c2c89e4b0fc2f76a47baf
umesh <umeshdataman@hotmail.com>
2019-04-24 20:13:50 +0530

    creation and edits

--- Tables.py
+++ Tables.py
@@ -1,8 +1,8 @@
-def tables()
+def tables():
     input_one=input("This is a multiplication table whatever number you type it will give u answer.. ")
     input_two=input("What is the end za of the table? .. ")
     product = input_one
     var2=0
     while var2 < int(input_two)+1:
         print(str(product) + " * " + str(var2) + " = " + str(int(var2)*int(product)))
-        var2 +=1
+        var2 +=1`
---
+++ hangman.py
@@ -0,0 +1,7 @@
+from string import ascii_lowercase
+from words import get_random_word
+
+
+def get_num_attempts():
+    """Get user
+    
